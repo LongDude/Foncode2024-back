038f64358c40887b8c760595d43d70d9c62c34d2
longdude <aeeaae@yaya.no>
2024-03-17 14:25:34 +0300

    and urls

--- Project/api/urls.py
+++ Project/api/urls.py
@@ -9,5 +9,9 @@ urlpatterns=[
     path('get_group_users', get_group_users),
     path('get_facullity_groups', get_faculity_groups),
     path('get_faculity', get_faculity_list),
-    path('register_user', register_user)
+    path('register_user', register_user),
+    path("get_courses_byuser", get_courses_byUser),
+    path("get_courses", get_courses),
+    path("get_course_content", get_course_content),
+    path("get_faculity_list", get_faculity_list)
 ]
